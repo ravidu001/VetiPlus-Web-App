ac22d0a4d8822176666df52318ae1d71d6b85c9c
Ramesh Peshala <rameshpeshala84@gmail.com>
2024-11-11 11:05:07 +0530

    add password change modal with functionality; implement modal styles and JavaScript for user interactions

--- client/pages/systemAdmin/profile.php
+++ client/pages/systemAdmin/profile.php
@@ -97,9 +97,33 @@ $current_page = basename($_SERVER['PHP_SELF']);
         </div>
             </div>
             <div class="profile_down">
-                 <button>Edit Profile</button>
+                 <button onclick="openModal()">Edit Profile</button>
             </div>
     </div>
 </div>
+
+<div id="passwordModal" class="modal">
+    <div class="modal-content">
+         <div class="modal-content-inside">
+         <h3>Do you want to change your password?</h3>
+         <span class="close" onclick="closeModal()">&times;</span>
+         </div>
+         <button onclick="showPasswordForm()">Yes</button>
+         <button onclick="closeModal()">No</button>
+         <div id="passwordForm" style="display: none;">
+               <h3>Change Password</h3>
+               <form onsubmit="changePassword(event)">
+                <input type="password" id="newPassword" placeholder="Enter new Password" required><br><br>
+                <input type="password" id="confirmPassword" placeholder="Confirm new Password" required><br><br>
+                <button type="submit">Change Password</button>
+               </form>
+         </div>
+         <div id="successMessage" style="display: none;">
+            <h3>Password chnaged successfully</h3>
+         </div>
+    </div>
+
+</div>
+<script src="../../assets/jsFIles/Admin/changePassword.js"></script>
 </body>
 </html>
