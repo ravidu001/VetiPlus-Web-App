f39029bf880b222f1f0b5cd4d73f12b5be116c14
Ramesh Peshala <rameshpeshala84@gmail.com>
2024-11-28 15:16:43 +0530

    refactor doctor profile styles; enhance layout with new table structure, improve responsiveness, and update button designs for better user experience

--- client/pages/systemAdmin/doctorProfile.php
+++ client/pages/systemAdmin/doctorProfile.php
@@ -22,27 +22,93 @@ $current_page = basename($_SERVER['PHP_SELF']);
     <?php include '../../components/common/admin/navbar.php'; ?>
 
     <section class="home">
-    <div class="doctor_profile">
-        <div class="doctor_profile_top">
-            <div class="doctor_profile_top_left">
-                <img src="../../assets/images/user.png" alt="">
-            </div>
-            <div class="doctor_profile_top_right">
+        <div class="doctor_profile">
+            <div class="doctor_profile_top">
+                <div class="doctor_profile_top_left">
+                    <img src="../../assets/images/user.png" alt="">
+                </div>
+                <!-- <div class="doctor_profile_top_right">
                 <h2>Full Name &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; :- &nbsp;&nbsp; Ramesh Peshala</h2>
                 <h2>Date of Birth &nbsp;&nbsp;&nbsp; :- &nbsp;&nbsp;2002/05/09</h2>
                 <h2>Gender &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; :- &nbsp;&nbsp; Male</h2>
                 <h2>Email &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:-&nbsp;&nbsp; rameshpeshala84@gmail.com</h2>
                 <h2>Phone Number &nbsp;:-&nbsp;&nbsp;&nbsp; 0762163506</h2>
                 <h2>NIC &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:- &nbsp;&nbsp;200212702901</h2>
+            </div> -->
+                <table>
+                    <tr>
+                        <td>
+                            <div class="regi-icon">
+                                <i class='bx bxs-user-circle icon'></i>
+                                <label for="name"> Name</label>
+                            </div>
+                        </td>
+                        <td>
+                            <h3>Ramesh Peshala</h3>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div class="regi-icon">
+                                <i class='bx bxs-envelope icon'></i>
+                                <label for="email">Email</label>
+                            </div>
+                        </td>
+                        <td>
+                            <h3>rameshpeshala84@gmail.com</h3>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div class="regi-icon">
+                                <i class='bx bxs-home icon'></i>
+                                <label for="address">Address</label>
+                            </div>
+                        </td>
+                        <td>
+                            <h3>419,Gangasirigama,Thissamaharama</h3>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div class="regi-icon">
+                                <i class='bx bxs-phone icon'></i>
+                                <label for="phone_number">Phone Number</label>
+                            </div>
+                        </td>
+                        <td>
+                            <h3>0762163506</h3>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div class="regi-icon">
+                                <i class='bx bxs-message-check icon'></i>
+                                <label for="nic">NIC</label>
+                            </div>
+                        </td>
+                        <td>
+                            <h3>200212702901</h3>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div class="regi-icon">
+                                <i class='bx bx-child icon'></i>
+                                <label for="gender">Gender</label>
+                            </div>
+                        </td>
+                        <td>
+                            <h3>Male</h3>
+                        </td>
+                    </tr>
+                </table>
+                <div class="doctor_profile_down">
+                    <button onclick="openConfirmModal()">Reset Password</button>
+                </div>
             </div>
         </div>
-        <div class="doctor_profile_down">
-            <div>
-                <button onclick="openConfirmModal()">Reset Password</button>
-            </div>
-        </div>
-    </div>
-    <div id="modal" class="modal-background" style="display: none;">
+        <div id="modal" class="modal-background" style="display: none;">
             <div class="modal-content">
                 <p>Do you want to reset your password?</p>
                 <div class="modal-buttons">
