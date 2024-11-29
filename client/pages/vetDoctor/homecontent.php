a07f0759d38ef6cb73e3cea39774b1c87d9a6b7c
ravidu001 <ravidupeiris2002@gmail.com>
2024-11-29 05:33:35 +0530

    complete reply button in homeNew.php

--- client/pages/vetDoctor/homecontent.php
+++ client/pages/vetDoctor/homecontent.php
@@ -10,13 +10,14 @@
             error_reporting(E_ALL);
 
             // Use `user_id` to retrieve the logged-in user’s information
-            $select = mysqli_query($conn, "SELECT * FROM `User` WHERE email = '$user_id'") or die('Query failed');
+            $select = mysqli_query($conn, "SELECT * FROM `vetDoctor` WHERE doctorID = '$user_id'") or die('Query failed');
             
             if (mysqli_num_rows($select) > 0) {
                 $fetch = mysqli_fetch_assoc($select);
-                echo "<h1>Dr. " . htmlspecialchars($fetch['email']) . "</h1>"; // assuming the field 'name' exists in the User table
+                echo "<h1>Dr. " . htmlspecialchars($fetch['fullName']) . "</h1>"; // assuming the field 'name' exists in the User table
             } else {
                 echo "<h1>User not found</h1>";
+                echo "<p>Please complete your profile</p>";
             }
         ?>
 
@@ -34,7 +35,7 @@
             <th>Assistance </th>
             <th>Location</th>
             <th>No. of Appointments </th>
-            <th> Button</th>
+            <th> </th>
         </thead>
         <tr>
             <td>1</td>
@@ -74,7 +75,7 @@
             <td>Kasun Perera</td>
             <td  >5</td>
             <td  >Good service</td>
-            <td style="text-align:center"><button class="button button1">Reply</button></td>
+            <td style="text-align:center"><button class="button button1 replyBtn">Reply</button></td>
         </tr>
 
         <tr>
@@ -86,7 +87,7 @@
             <td>Sadun Perera</td>
             <td  >5</td>
             <td  >Good service</td>
-            <td style="text-align:center"><button class="button button1">Reply</button></td>
+            <td style="text-align:center"><button class="button button1 replyBtn">Reply</button></td>
         </tr>
 
         <tr>
@@ -98,8 +99,10 @@
             <td>Chamara Perera</td>
             <td  >5</td>
             <td  >Good service</td>
-            <td style="text-align:center"><button class="button button1">Reply</button></td>
+            <td style="text-align:center"><button class="button button1 replyBtn">Reply</button></td>
         </tr>
     
     </table>
 </div>
+
+<script src="../../assets/jsFIles/vetDocotor/home.js"></script>
